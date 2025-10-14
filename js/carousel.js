0b2564162228f4d422a7616cc2e688376a9a8080
BigDavi220V <davi_santiagoo@hotmail.com>
2025-10-14 10:49:39 -0300

    Fiz o metodo do carrosel funcionar e coloquei botoes para ir e voltar no carrosel

--- js/carousel.js
+++ js/carousel.js
@@ -1,45 +1,75 @@
-// const imgs = document.getElementById("carousel");
-// const img = document.querySelectorAll("#carousel-title");
-
-// let idx = 0;
-
-// function carrosel (){
-//     idx++;
-//     if(idx > img.length -1){
-//         idx = 0;
-//     }
-// }
-
-// imgs.style.transform = `translateX(${-idx * 500}px)`;
-
-// setInterval(carrosel, 1800);
-//carousel
-
 //Array storage class
-let carouselArr = [];
+
+
 
 
 //class Carousel
+let carouselArr = [];
+
 class Carousel {
+    constructor(image, text, link) {
+        this.image = image;
+        this.text = text;
+        this.link = link;
+    }
 
-    
-      
-    static Start(arr){
-        if(arr){
+    static Start(arr) {
+        if (arr && arr.length > 0) {
+            Carousel._sequence = 0;
+            Carousel._size = arr.length;
+            Carousel._arr = arr;
+            Carousel.Show(Carousel._sequence); // mostra primeira imagem
+            Carousel._interval = setInterval(() => Carousel.Next(), 2000);
 
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
-            
+            // adiciona eventos dos botões
+            document.getElementById("next").addEventListener("click", () => {
+                Carousel.Next(true);
+            });
+
+            document.getElementById("prev").addEventListener("click", () => {
+                Carousel.Prev(true);
+            });
         } else {
-            throw "Method Start need a Array Variable.";
+            throw "ERRO!";
         }
     }
 
-    static Next(){
-        
+    static Show(index) {
+        let item = Carousel._arr[index];
+
+        // atualiza imagem
+        let carouselDiv = document.getElementById("carousel");
+        carouselDiv.innerHTML = `<a href="${item.link}">
+                                    <img src="./img/${item.image}" alt="carousel image">
+                                 </a>`;
+
+        // atualiza título
+        let titleDiv = document.getElementById("carousel-title");
+        titleDiv.innerHTML = `<p>${item.text}</p>`;
     }
-};
+
+    static Next(stopAuto = false) {
+        Carousel._sequence++;
+        if (Carousel._sequence >= Carousel._size) {
+            Carousel._sequence = 0;
+        }
+        Carousel.Show(Carousel._sequence);
+
+        if (stopAuto) Carousel.ResetInterval();
+    }
+
+    static Prev(stopAuto = false) {
+        Carousel._sequence--;
+        if (Carousel._sequence < 0) {
+            Carousel._sequence = Carousel._size - 1;
+        }
+        Carousel.Show(Carousel._sequence);
+
+        if (stopAuto) Carousel.ResetInterval();
+    }
+
+    static ResetInterval() {
+        clearInterval(Carousel._interval);
+        Carousel._interval = setInterval(() => Carousel.Next(), 5000);
+    }
+}
