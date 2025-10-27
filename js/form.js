943170454938c5beae8f48415db1cd2867fd9724
BigDavi220V <davi_santiagoo@hotmail.com>
2025-10-27 09:43:37 -0300

    Revert " pagina lancamento e contato concluido"
    This reverts commit c0d70849de6d4de33666e34bd6f358d59e66f6d2.

--- js/form.js
+++ js/form.js
@@ -2,18 +2,10 @@
 //class contato
 
 class contato {
-    constructor(nome, sobrenome, email, cpf, telefone, contato) {
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.email = email;
-        this.cpf = cpf;
-        this.telefone = telefone;
-        this.contato = contato;
-    }
+    
 }
 
 function Post(form) {
-    event.preventDefault();
 
   let data = new contato(form.elements.namedItem("nome").value,
             form.elements.namedItem("sobrenome").value, 
@@ -21,11 +13,7 @@ function Post(form) {
             form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
             form.elements.namedItem("contato").value);
-
-            console.log(data);
-    alert(`Obrigado sr(a) ${data.nome} ${data.sobrenome}, os seus dados foram encaminhados com sucesso`);
-    form.reset();
-    return false;
+  
 }
 
 function Enviar() {
